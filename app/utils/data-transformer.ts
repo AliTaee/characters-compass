dbdefbb3a842d9bd1cb4fedbb2220f4b055ab607
AliTaee <ali.taee.72@gmail.com>
2024-11-04 19:44:48 +0100

    refactor data transformer

--- app/utils/data-transformer.ts
+++ app/utils/data-transformer.ts
@@ -4,22 +4,24 @@ const PokemonDataTransformer = (rowDataList:any): DataTransformer[] => {
     return rowDataList?.map((data:any) => {
         const id = data.url.split('/').filter(Boolean).pop()
         const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png` 
+        const { name, url } = data
         return {
             id,
-            name: data.name,
+            name,
             image,
-            url: data.url,
+            url,
         }
     })
 }
 
 const RickAndMortyDataTransformer = (rowDataList:any): DataTransformer[] => {
     return rowDataList?.map((data:any) => {
+        const { name, image, url, id } = data
         return {
-            id: data.id,
-            name: data.name,
-            image: data.image,
-            url: data.url,
+            id,
+            name,
+            image,
+            url,
         }
     })
 }
