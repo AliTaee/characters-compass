3935b3b4e72ac46b3bd01d336ac712bc047d27c0
AliTaee <ali.taee.72@gmail.com>
2024-11-04 15:49:43 +0100

    replace footer text

--- app/components/Application/Footer.vue
+++ app/components/Application/Footer.vue
@@ -1,7 +1,18 @@
 <template>
   <footer class="bg-zinc-100 border-t border-t-zinc-200 dark:bg-emerald-700 dark:border-t-emerald-600 py-8">
-    <UContainer>
-      Footer content
+    <UContainer class="text-center">
+      Design and develop by Ali Taei &copy; {{ year }} - With <UIcon name="i-logos:nuxt-icon" class="w-4 h-4" />
+      and <UIcon name="i-heroicons-solid:heart" class="w-4 h-4" />
     </UContainer>
   </footer>
 </template>
+
+<script>
+export default {
+  computed: {
+    year() {
+      return new Date().getFullYear();
+    }
+  }
+}
+</script>
