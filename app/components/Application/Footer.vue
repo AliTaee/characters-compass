e084bc45c009fcb49519ad091ba9759167941492
AliTaee <ali.taee.72@gmail.com>
2024-11-04 14:46:37 +0100

    improve color theme of application

--- app/components/Application/Footer.vue
+++ app/components/Application/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-zinc-100 border-t border-t-zinc-200 dark:bg-zinc-700 dark:border-t-zinc-600 py-8">
+  <footer class="bg-zinc-100 border-t border-t-zinc-200 dark:bg-emerald-700 dark:border-t-emerald-600 py-8">
     <UContainer>
       Footer content
     </UContainer>
