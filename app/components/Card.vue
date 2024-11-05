a3cc0afd231524b63d3cf75ea862b16653385a8d
AliTaee <ali.taee.72@gmail.com>
2024-11-05 18:21:19 +0100

    handle long text on card components

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -19,7 +19,7 @@ watch(() => props.img, (newImg) => {
   <section
     class="rounded shadow shadow-slate-100 dark:shadow-slate-900 bg-slate-200 dark:bg-slate-800 border border-slate-500 dark:border-slate-600">
     <header class="border-b border-b-slate-200 dark:border-b-slate-600 p-4">
-      <h2 class="text-center text-2xl font-bold tracking-wide text-slate-700 dark:text-slate-300">
+      <h2 class="text-center text-2xl font-bold tracking-wide text-slate-700 dark:text-slate-300 truncate">
         {{ props.title }}
       </h2>
     </header>
