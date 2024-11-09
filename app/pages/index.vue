e787cbb44c07a5276efb3dea51bc8f95018a752a
AliTaee <ali.taee.72@gmail.com>
2024-11-09 12:58:46 +0100

    optimize image size of main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,10 +8,10 @@
       </header>
       <div class="grid gap-4 xl:grid-cols-2">
         <ULink to="/rick-and-morty">
-          <Card title="Rick and Morty" img="/images/wallpaper/rick-and-morty.png" image-size="cover" />
+          <Card title="Rick and Morty" img="/images/wallpaper/rick-and-morty.jpg" image-size="cover" />
         </ULink>
         <ULink to="/pokemon">
-          <Card title="Pokemon" img="/images/wallpaper/pokemon.png" image-size="cover" />
+          <Card title="Pokemon" img="/images/wallpaper/pokemon.jpg" image-size="cover" />
         </ULink>
       </div>
     </UContainer>
