d7a91690690657158e8b84ac4fc13dba18b5fcbd
AliTaee <ali.taee.72@gmail.com>
2024-11-10 18:58:27 +0100

    imporve list view in char list page

---
+++ app/components/Char/List/Item.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  link: string
+}>()
+</script>
+
+<template>
+  <section
+    class="flex justify-between items-center p-4 rounded shadow shadow-slate-100 dark:shadow-slate-900 bg-slate-200 dark:bg-slate-800 border border-slate-500 dark:border-slate-600 mb-2"
+  >
+    <header class="border-b-slate-200 dark:border-b-slate-600">
+      <h2 data-testid="card-title" class="text-center text-base font-bold tracking-wide text-slate-700 dark:text-slate-300 truncate">
+        {{ props.title }}
+      </h2>
+    </header>
+    <ULink :to="props.link">
+      Detail page
+    </ULink>
+  </section>
+</template>
--- app/components/Char/List/List.vue
+++ app/components/Char/List/List.vue
@@ -7,17 +7,24 @@ const props = defineProps<{
 }>()
 
 const gridColumnsClass = computed(() => {
-  return props.columns === 1 ? 'xl:grid-cols-1' : 'xl:grid-cols-3'
+  return props.columns === 1 ? 'grid-cols-1' : 'grid-cols-1 md:grid-cols-2 xl:grid-cols-3'
 })
 </script>
 
 <template>
-  <div v-if="props.charList.length" class="grid gap-4" :class="[gridColumnsClass]">
+  <div v-show="props.charList.length && props.columns !== 1" class="grid gap-4" :class="[gridColumnsClass]">
     <ULink v-for="char in props.charList" :key="char.id" :to="char.url">
       <Card :title="char.name" :img="char.image" image-size="cover" />
     </ULink>
   </div>
-  <div v-else>
+  <CharListItem
+    v-for="char in props.charList"
+    v-show="props.charList.length && props.columns === 1"
+    :key="char.id"
+    :title="char.name"
+    :link="char.url"
+  />
+  <div v-show="props.charList.length === 0">
     <p> No characters found.</p>
   </div>
 </template>
