8685b1b531bc51ffd39d7743d153bbd6d32ea00b
AliTaee <ali.taee.72@gmail.com>
2024-11-10 19:27:08 +0100

    fix: responsive char name text on list view

--- app/components/Char/List/Item.vue
+++ app/components/Char/List/Item.vue
@@ -7,10 +7,10 @@ const props = defineProps<{
 
 <template>
   <section
-    class="flex justify-between items-center p-4 rounded shadow shadow-slate-100 dark:shadow-slate-900 bg-slate-200 dark:bg-slate-800 border border-slate-500 dark:border-slate-600 mb-2"
+    class="flex justify-between items-center gap-2 p-4 rounded shadow shadow-slate-100 dark:shadow-slate-900 bg-slate-200 dark:bg-slate-800 border border-slate-500 dark:border-slate-600 mb-2"
   >
-    <header class="border-b-slate-200 dark:border-b-slate-600">
-      <h2 data-testid="card-title" class="text-center text-base font-bold tracking-wide text-slate-700 dark:text-slate-300 truncate">
+    <header class="border-b-slate-200 dark:border-b-slate-600 truncate max-w-48 md:max-w-full">
+      <h2 data-testid="card-title" class="text-center text-base font-bold tracking-wide text-slate-700 dark:text-slate-300">
         {{ props.title }}
       </h2>
     </header>
