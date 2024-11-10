3c3e639146c37b292d382e66199ba2e684962ec4
AliTaee <ali.taee.72@gmail.com>
2024-11-10 18:33:57 +0100

    feat: implement responsive menu

--- app/components/Application/Header/Header.vue
+++ app/components/Application/Header/Header.vue
@@ -1,21 +1,37 @@
+<script setup lang="ts">
+const isMenuOpen = ref(false)
+const toggleMenu = () => (isMenuOpen.value = !isMenuOpen.value)
+
+const horizontalLinks = [
+  { label: 'Home', to: '/' },
+  { label: 'Rick and Morty', to: '/rick-and-morty' },
+  { label: 'Pokemon', to: '/pokemon' },
+  { label: 'Elden Ring', to: '/elden-ring' },
+]
+const verticalLinks = [...horizontalLinks]
+</script>
+
 <template>
   <header class="bg-zinc-100 border-b border-b-zinc-200 dark:bg-emerald-700 dark:border-b-emerald-600 py-8">
     <UContainer class="flex items-center">
       <nav>
-        <ul class="flex gap-2">
-          <li>
-            <ApplicationHeaderLink to="/" label="Home" />
-          </li>
-          <li>
-            <ApplicationHeaderLink to="/rick-and-morty" label="Rick and Morty" />
-          </li>
-          <li>
-            <ApplicationHeaderLink to="/pokemon" label="Pokemon" />
-          </li>
-          <li>
-            <ApplicationHeaderLink to="/elden-ring" label="Elden Ring" />
-          </li>
-        </ul>
+        <UButton class="block md:hidden" @click="toggleMenu()">
+          <Icon :name="isMenuOpen ? 'pajamas:close' : 'pajamas:hamburger'" class="w-4 h-4 mt-1" />
+        </UButton>
+        <div class="hidden md:flex">
+          <UHorizontalNavigation :links="horizontalLinks">
+            <template #default="{ link }">
+              <span class="group-hover:text-primary text-cyan-50 relative">{{ link.label }}</span>
+            </template>
+          </UHorizontalNavigation>
+        </div>
+        <div v-if="isMenuOpen" class="flex flex-col md:hidden p-4">
+          <UVerticalNavigation :links="verticalLinks">
+            <template #default="{ link }">
+              <span class="group-hover:text-primary text-cyan-50 relative">{{ link.label }}</span>
+            </template>
+          </UVerticalNavigation>
+        </div>
       </nav>
     </UContainer>
   </header>
