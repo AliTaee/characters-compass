dfbc073cc77d9df19a17e035c9c12f490e9b47e1
AliTaee <ali.taee.72@gmail.com>
2024-11-10 23:11:37 +0100

    fix: char long text title on mobile

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -22,7 +22,7 @@ watch(() => props.img, (newImg) => {
     class="rounded shadow shadow-slate-100 dark:shadow-slate-900 bg-slate-200 dark:bg-slate-800 border border-slate-500 dark:border-slate-600"
   >
     <header class="border-b border-b-slate-200 dark:border-b-slate-600 p-4">
-      <h2 data-testid="card-title" class="text-center text-2xl font-bold tracking-wide text-slate-700 dark:text-slate-300 truncate">
+      <h2 data-testid="card-title" class="text-center text-base md:text-2xl font-bold tracking-wide text-slate-700 dark:text-slate-300 truncate">
         {{ props.title }}
       </h2>
     </header>
